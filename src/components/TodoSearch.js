9cead9177a336193bfdef5445d9e07da90dbac91
Dan Santos <dan.santos.hd@outlook.com>
2022-06-27 22:35:38 -0400

    Se agrega status a search

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import './components.css';
 
 function TodoSearch() {
+  const [searchText, setSearchText] = React.useState('');
+
   const onChangeText = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
+    setSearchText(event.target.value);
   };
+
+  console.log(searchText);
 
   return (
     <input
       className='search-bar'
       placeholder="Nueva Nota"
+      value={searchText}
       onChange={onChangeText}
     />
   );
